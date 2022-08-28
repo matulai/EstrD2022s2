980fc4a8863d1e8da0234092b67175836b34a240
Matias Laime <59126563+matulai@users.noreply.github.com>
2022-08-28 15:38:37 -0300

    Update práctica_1.hs

--- PRÁCTICA_1/práctica_1.hs
+++ PRÁCTICA_1/práctica_1.hs
@@ -1,8 +1,8 @@
 {-	
 ----------------------------------------------
-||	PRÁCTICA N°1 |TIPOS ALGEBRAICOS|		||
-||	Alumno: Matias Laime					||
-||	Fecha De Inicio: 19/08/2022				||
+||	PRÁCTICA N°1 |TIPOS ALGEBRAICOS|    ||
+||	Alumno: Matias Laime		    ||
+||	Fecha De Inicio: 19/08/2022	    ||
 ----------------------------------------------
 -}
 --2.|NÚMEROS ENTEROS|
@@ -90,7 +90,7 @@ oBien _ _ = True
 --4.|REGISTROS|
 	--1
 data Persona = P String Int deriving Show
-			   --Nombre Edad
+	       --Nombre Edad
 matias = P "Matias" 21
 leandro = P "Leandro" 23
 
@@ -111,15 +111,15 @@ esMayorQueLaOtra (P n e) (P x y) = e > y
 
 laQueEsMayor :: Persona -> Persona -> Persona
 laQueEsMayor x y = if (esMayorQueLaOtra x y)
-				   then x
-				   else y
+			then x
+			else y
 
 	--2
 data TipoDePokemon = Agua | Electrico | Lucha | Tierra deriving Show 
 data Pokemon = Pk TipoDePokemon Int deriving Show
-							  --Porcentaje de energia del pokemon
+		              --Porcentaje de energia del pokemon
 data Entrenador = Ent String Pokemon Pokemon deriving Show
-					--Nombre
+		    --Nombre
 
 pikachu = Pk Electrico 76
 machoke = Pk Lucha 89
