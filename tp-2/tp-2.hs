47252c9a764aee27786088841f136e3c58378f7c
Matias Laime <matulai1221@gmail.com>
2022-09-06 01:33:38 -0300

    Finalizado

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -2,7 +2,7 @@
 ------------------------------------------------------
 ||	PRÁCTICA N°2 |LISTAS Y RECURSIÓN ESTRUCTURAL|	||
 ||	Alumno: Matias Laime					        ||
-||	Fecha De Inicio: 19/08/2022				        ||
+||	Fecha De Inicio: 28/08/2022				        ||
 ------------------------------------------------------
 -}
 --1.|RECURSIÓN SOBRE LISTAS|
@@ -17,15 +17,15 @@ longitud (_ : xs) = 1 + longitud xs
     --3
 sucesores ::  [Int] -> [Int]
 sucesores []       = []
-sucesores (x : xs) = x - 1 : sucesores xs
+sucesores (x : xs) = x + 1 : sucesores xs
     --4
 conjuncion :: [Bool] -> Bool
---PRECONDICIÓN: La lista no debe tener al menos un elemento.
+--PRECONDICIÓN: La lista debe tener al menos un elemento.
 conjuncion (x : []) = x
 conjuncion (x : xs) = x && conjuncion xs
     --5
 disyuncion :: [Bool] -> Bool
---PRECONDICIÓN: La lista no debe tener al menos un elemento.
+--PRECONDICIÓN: La lista debe tener al menos un elemento.
 disyuncion (x : []) = x
 disyuncion (x : xs) = x || disyuncion xs 
     --6
@@ -60,11 +60,6 @@ agregar (x : xs) ys = x : agregar xs ys
 reversa :: [a] -> [a]
 reversa []       = [] 
 reversa (x : xs)     = (reversa xs) ++ [x]
-
-ultimo :: [a] -> a
---PRECONDICIÓN: La lista no debe ser vacia
-ultimo (x : []) = x
-ultimo (x : xs) = ultimo xs
     --14
 zipMaximos :: [Int] -> [Int] -> [Int]
 zipMaximos [] []             = []
@@ -85,6 +80,7 @@ elMinimo (x : y : xs) = elMinimo ((primerElementoSi_Sino x (x < y) y) : xs)
 --2.|RECURSIÓN SOBRE NÚMEROS|
     --1
 factorial :: Int -> Int
+--PRECONDICIÓN: El entero debe ser mayor o igual a 0.
 factorial 0 = 1
 factorial x = x * factorial (x - 1)
     --2
@@ -109,44 +105,45 @@ sinLosPrimeros x (y : ys) = sinLosPrimeros (x - 1) ys
 --3.|REGISTROS|
     --1
 data Persona = P String Int 
+--Funciones Observadoras--
 edad :: Persona -> Int
 edad (P _ i) = i
-
+-----------------------------------------------------
 mayoresA :: Int -> [Persona] -> [Persona]
 mayoresA _ []       = []
 mayoresA x (y : ys) = listaDeElemento_Si_SinoListaVacia y (x < (edad y)) ++ mayoresA x ys
-
+-----------------------------------------------------
 promedioEdad :: [Persona] -> Int 
 --PRECONDICIÓN : La lista posee al menos una persona
 promedioEdad (x : []) = edad x
 promedioEdad (x : xs) = div (edad x + promedioEdad xs) 2
-
+-----------------------------------------------------
 elMasViejo :: [Persona] -> Persona
 --PRECONDICIÓN : La lista posee al menos una persona
 elMasViejo (x : []) = x
 elMasViejo (x : y : xs) = elMasViejo ((primerElementoSi_Sino x (edad x > edad y) y) : xs)
-    
+
     --2
 data TipoDePokemon = Agua | Fuego | Planta deriving Show
 data Pokemon = Pk TipoDePokemon Int
 data Entrenador = Ent String [Pokemon]
 --Funciones observadoras--
-tipoDePokemonDe :: Pokemon -> TipoDePokemon
-tipoDePokemonDe (Pk t _) = t
+tipoDe :: Pokemon -> TipoDePokemon
+tipoDe (Pk t _) = t
 
 pokemonesDe :: Entrenador -> [Pokemon]
 pokemonesDe (Ent _ ps) = ps
---------------------------
+-----------------------------------------------------
 
 cantPokemon :: Entrenador -> Int
 cantPokemon e = longitud (pokemonesDe e)
---------------------------
+-----------------------------------------------------
 cantPokemonDe :: TipoDePokemon -> Entrenador -> Int
 cantPokemonDe tp e = cantDePokemonesDeTipo_En_ tp (pokemonesDe e)
 
 cantDePokemonesDeTipo_En_ :: TipoDePokemon -> [Pokemon] -> Int
 cantDePokemonesDeTipo_En_ _  []         = 0
-cantDePokemonesDeTipo_En_ tp (p : ps)   = unoSi(sonDelMismoTipoDePokemon tp (tipoDePokemonDe p)) + cantDePokemonesDeTipo_En_ tp ps
+cantDePokemonesDeTipo_En_ tp (p : ps)   = unoSi(sonDelMismoTipoDePokemon tp (tipoDe p)) + cantDePokemonesDeTipo_En_ tp ps
 
 sonDelMismoTipoDePokemon :: TipoDePokemon -> TipoDePokemon -> Bool
 sonDelMismoTipoDePokemon Agua   Agua    = True
@@ -154,41 +151,51 @@ sonDelMismoTipoDePokemon Fuego  Fuego   = True
 sonDelMismoTipoDePokemon Planta Planta  = True
 
 unoSi :: Bool -> Int
-unoSi True = 1
+unoSi True  = 1
 unoSi False = 0
---------------------------
--- losQueLeGanan :: TipoDePokemon -> Entrenador -> Entrenador -> Int
--- losQueLeGanan tp (Ent _ ps1) (Ent _ ps2) = cantDePokemonesDeTipo_En_ tp ps1 - cantDePokemonesDeTipo_En_ (esEficazContra tp) ps2 + cantDePokemonesDeTipo_En_ tp ps1
+-----------------------------------------------------
+losQueLeGanan :: TipoDePokemon -> Entrenador -> Entrenador -> Int
+losQueLeGanan tp e e1 = losDeTipo_En_QueLeGananATodos tp (pokemonesDe e) (pokemonesDe e1)
 
--- esEficazContra :: TipoDePokemon -> TipoDePokemon
--- esEficazContra  Agua    = Fuego
--- esEficazContra  Fuego   = Planta
--- esEficazContra  Planta  = Agua
--- --------------------------
--- esMaestroPokemon :: Entrenador -> Bool
--- esMaestroPokemon e = hayDeTodosLosTiposDePokemon Agua (pokemonesDe e)
+losDeTipo_En_QueLeGananATodos :: TipoDePokemon -> [Pokemon] -> [Pokemon] -> Int
+losDeTipo_En_QueLeGananATodos _  []   _      = 0
+losDeTipo_En_QueLeGananATodos _  _    []     = 0
+losDeTipo_En_QueLeGananATodos tp (x : xs) ys = unoSi (sonDelMismoTipoDePokemon (tipoDe x) tp && leGanaATodos x ys) + losDeTipo_En_QueLeGananATodos tp xs ys
 
--- hayDeTodosLosTiposDePokemon :: Pokemon -> [Pokemon] -> Bool
--- hayDeTodosLosTiposDePokemon 
--- hayDeTodosLosTiposDePokemon p ps = hayPokemonDe p ps && hayPokemonDe siguienteTipoDePokemon 
+leGanaATodos :: Pokemon -> [Pokemon] -> Bool
+leGanaATodos _ []         = True
+leGanaATodos p (p1 : p1s) = superaA p p1 && leGanaATodos p p1s
 
--- siguienteTipoDePokemon :: TipoDePokemon -> TipoDePokemon
--- siguienteTipoDePokemon Planta = Agua
--- siguienteTipoDePokemon Agua   = Fuego 
--- siguienteTipoDePokemon Fuego  = Planta
+-----------------------------------------------------
+esMaestroPokemon :: Entrenador -> Bool
+esMaestroPokemon e = hayPokemonDe Agua (pokemonesDe e) && 
+                     hayPokemonDe Planta (pokemonesDe e) &&
+                     hayPokemonDe Fuego (pokemonesDe e)
 
--- hayPokemonDe :: TipoDePokemon -> [Pokemon] -> Bool
--- hayPokemonDe _      []        = False
--- hayPokemonDe Planta _         = False
--- hayPokemonDe tp     (p : ps)  = sonDelMismoTipoDePokemon tp (tipoDePokemonDe p) || hayPokemonDe tp ps
+hayPokemonDe :: TipoDePokemon -> [Pokemon] -> Bool
+hayPokemonDe _      []        = False
+hayPokemonDe Planta _         = False
+hayPokemonDe tp     (p : ps)  = sonDelMismoTipoDePokemon tp (tipoDePokemonDe p) || hayPokemonDe tp ps
 
     --3
 data Seniority = Junior | SemiSenior | Senior deriving Show
 data Proyecto = Pry String
 data Rol = Developer Seniority Proyecto | Management Seniority Proyecto
 data Empresa = Emp [Rol]
+--Funciones Observadoras--
+nombreDeProyecto :: Proyecto -> String
+nombreDeProyecto (Pry n) = n 
 
---------------------------
+proyectoDe :: Rol -> Proyecto
+proyectoDe (Developer _ p) = p 
+proyectoDe (Management _ p) = p
+
+rol :: Empresa -> [Rol] 
+rol (Emp r) = r 
+
+seniorityDe :: Rol -> Seniority
+seniorityDe (Developer s _) = s
+-----------------------------------------------------
 
 proyectos :: Empresa -> [Proyecto]
 proyectos e = sinProyectosRepetidos (proyectosEn (rol e))
@@ -209,7 +216,7 @@ existeProyectoEn :: Proyecto -> [Proyecto] -> Bool
 existeProyectoEn _ []         = True
 existeProyectoEn p (p1 : p1s) = (nombreDeProyecto p == (nombreDeProyecto p1)) && existeProyectoEn p p1s
 
---------------------------
+-----------------------------------------------------
 
 losDevSenior :: Empresa -> [Proyecto] -> Int
 losDevSenior e ps = cantidadTrabajandoEnAlgunoDe (soloDesarrolladoresSenior (rol e)) ps 
@@ -235,12 +242,12 @@ igualSeniority SemiSenior SemiSenior = True
 igualSeniority Junior     Junior     = True
 igualSeniority _          _          = False
 
---------------------------
+-----------------------------------------------------
 
 -- cantQueTrabajanEn :: [Proyecto] -> Empresa -> Int --
 -- Función ya hecha. "cantidadTrabajandoEnAlgunoDe"
 
---------------------------
+-----------------------------------------------------
 
 asignadosPorProyecto :: Empresa -> [(Proyecto, Int)]
 asignadosPorProyecto e = proyectoYCantidadTrabajandoEn (proyectos e) (rol e)
@@ -248,17 +255,3 @@ asignadosPorProyecto e = proyectoYCantidadTrabajandoEn (proyectos e) (rol e)
 proyectoYCantidadTrabajandoEn :: [Proyecto] -> [Rol] -> [(Proyecto, Int)]
 proyectoYCantidadTrabajandoEn []       _  = []
 proyectoYCantidadTrabajandoEn (p : ps) rs = (p, cantidadTrabajandoEnAlgunoDe rs [p]) : proyectoYCantidadTrabajandoEn ps rs
-
---Funciones Observadoras--
-nombreDeProyecto :: Proyecto -> String
-nombreDeProyecto (Pry n) = n 
-
-proyectoDe :: Rol -> Proyecto
-proyectoDe (Developer _ p) = p 
-proyectoDe (Management _ p) = p
-
-rol :: Empresa -> [Rol] 
-rol (Emp r) = r 
-
-seniorityDe :: Rol -> Seniority
-seniorityDe (Developer s _) = s
